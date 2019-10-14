59398b60778ff1e023ccfe3ca227b450af2f4795
Sajan Gurung <sjnkon@gmail.com>
2019-10-14 22:46:42 +1100

    add bid card

---
+++ src/components/bid-card/BidCard.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import './BidCard.scss';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  card: {
+    minWidth: 275
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)'
+  },
+  title: {
+    fontSize: 14
+  },
+  pos: {
+    marginBottom: 12
+  }
+});
+
+function BidCard() {
+  const classes = useStyles();
+  const bull = <span className={classes.bullet}>•</span>;
+  return (
+    <Card className={classes.card}>
+      <CardContent>
+        <Typography
+          className={classes.title}
+          color="textSecondary"
+          gutterBottom
+        >
+          Current Highest Bid
+        </Typography>
+        <Typography variant="h5" component="h2">
+          $1000
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+          by tom
+        </Typography>
+        <Typography variant="body2" component="p">
+          04:39 remaining
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Enter bid room</Button>
+      </CardActions>
+    </Card>
+  );
+}
+
+export default BidCard;
